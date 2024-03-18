d27b4b604c7fa2e865ecab711666ad394f6e1bd3
angeiradu <a.iradukund2@alustudent.com>
2024-03-18 07:45:26 -0700

    list_item

---
+++ list-item/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Listitem from './components/listitem.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Listitem />
+    </div>
+  );
+}
+
+export default App;
---
+++ list-item/src/components/listitem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const listitem = () => {
+  return (
+    <div className='bg-[#f0f6ff] h-screen grid place-items-center'>
+        <div className='bg-[#ffffff]  flex gap-6'>
+          <p>Ranking</p>
+          <p>Player name</p>
+          <p>Age group</p>
+          <p>height</p>
+          <p>Position</p>
+          <p>Star rating</p>
+        </div>
+    </div>
+  )
+}
+
+export default listitem
